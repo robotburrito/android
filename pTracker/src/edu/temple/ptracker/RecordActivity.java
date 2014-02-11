b7c3da4b016b8081f486c47768bb2b6c8af07013
Vincent Martin <you_know_me@temple.edu>
2014-02-11 17:21:03 -0500

    Oh my god it writes data to a file...

--- pTracker/src/edu/temple/ptracker/RecordActivity.java
+++ pTracker/src/edu/temple/ptracker/RecordActivity.java
@@ -2,22 +2,25 @@ package edu.temple.ptracker;
 
 
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Date;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.view.Menu;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
@@ -28,24 +31,41 @@ public class RecordActivity extends Activity implements SensorEventListener {
 	private View view;
 	private long lastUpdate;
 	 
-	//Setup file stream.
-	//FileOutputStream outputFile;
+	 Date today = new Date();
+	
+	//Did some math... If I sample every 16.67 ms then I am sampling at 60 Hz.
+	//long millisecondsBetweenSamples = 17;
+	
+	//Declare the date object for time stamping.
+	String fileNameOutput = Long.toString(today.getTime()) + ".csv";
+	//String fileNameOutput = "pleasework.csv";
+	
+	File sdCard = Environment.getExternalStorageDirectory();
+	File directory = new File (sdCard.getAbsolutePath() + "/pTracker");
+	File file = new File(directory, fileNameOutput);
 	
 	//Declare Button Objects
 	Button stopRecordButton;
 	
-	//int refreshRate = 500;
+	//Setup file stream by first creating fileNameOutput to automatically generate the filename to save data to. 
+	//Once the file name is created then declare an outputStream
+	
+	//FileOutputStream outputStream;
+
+	
+	
+	
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_record);
-		//view = findViewById(R.id.endRecordButton);
 		
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		lastUpdate = System.currentTimeMillis();
-	
+
 		
 		//Setup the buttons
 		stopRecordButton = (Button) findViewById(R.id.endRecordButton);
@@ -59,6 +79,7 @@ public class RecordActivity extends Activity implements SensorEventListener {
 		  	public void onClick(View v) {
 		  		finish();}
 		});
+		
 		
 	}
 	
@@ -98,30 +119,44 @@ public class RecordActivity extends Activity implements SensorEventListener {
 	    
 	    //Update the fields if the above doesn't dump out.
 	    lastUpdate = actualTime;
-	    updateXYZFields(x,y,z);
+	    try {
+			updateXYZFields(x,y,z);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	    
 	    return; 
 	    
 	  }
 	  
-	public void updateXYZFields(float x, float y, float z){
+	public void updateXYZFields(float x, float y, float z) throws IOException {
+		  
 		  TextView xValueText = (TextView) findViewById(R.id.textXValue);
 		  TextView yValueText = (TextView) findViewById(R.id.textYValue);
 		  TextView zValueText = (TextView) findViewById(R.id.textZValue);
+		  TextView dateValueText = (TextView) findViewById(R.id.textTimeElapsed);
+		  Long timeStampNow = System.currentTimeMillis();
+		  
 
 		  
 		  //Convert the values passed to strings so that they can be set to the text fields.
 		  String xStringValue = Float.toString(x);
 		  String yStringValue = Float.toString(y);
 		  String zStringValue = Float.toString(z);
+		  String dateStringValue = Long.toString(timeStampNow);
 		  
 		  //Set values to the text fields.
 		  xValueText.setText(xStringValue);
 		  yValueText.setText(yStringValue);
 		  zValueText.setText(zStringValue);		
+		  dateValueText.setText(dateStringValue);
 		  
-		  //Let us go ahead and try to write this data to a file on the device.
-		  //String FILENAME = "XYZ_output.txt"; 
+		  FileOutputStream fOut = new FileOutputStream(file, true);
+		  PrintWriter pWriter = new PrintWriter(fOut);
+		  pWriter.printf("%d,%f,%f,%f",timeStampNow,x,y,z );
+		  //pWriter.printf("FUCK FUCK FUCK");
+		  pWriter.close();
 		  
 	  }
 	
@@ -129,10 +164,16 @@ public class RecordActivity extends Activity implements SensorEventListener {
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
 	
-	public void endRecording(){
-		//Put some code in here to do file closings and to save data.
+	public void endRecording() throws IOException{
+		
+		
 	}
 
+	public void openFileWithDate()
+	{
+		
+	
+	}
 	protected void onResume() {
 	    super.onResume();
 	    // register this class as a listener for the orientation and
